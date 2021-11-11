48b3caea41cd09d0325f546f1804a89b1309f238
Dhiraj Sonawane <dhiraj.sonawane@coupa.com>
2021-11-11 21:25:25 +0530

    Created deleteoption to delete portfolio item

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -44,4 +44,16 @@ class PortfoliosController < ApplicationController
       @portfolio_item = Portfolio.find(params[:id])
     end
 
+    def destroy
+
+      @portfolio_item = Portfolio.find(params[:id])
+
+      @portfolio_item.destroy
+
+      respond_to do |format|
+        format.html { redirect_to portfolios_url, notice: "Portfolio item successfully destroyed." }
+      #  format.json { head :no_content }
+      end
+    end
+
 end
