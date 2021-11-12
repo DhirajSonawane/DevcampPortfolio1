1e746a13f3476d71c5ec8f68845811f0b7df28f7
Dhiraj Sonawane <dhiraj.sonawane@coupa.com>
2021-11-12 12:40:08 +0530

    Integrated friendly routes for blogg pages

--- app/controllers/bloggs_controller.rb
+++ app/controllers/bloggs_controller.rb
@@ -59,7 +59,8 @@ class BloggsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blogg
-      @blogg = Blogg.find(params[:id])
+      #@blogg = Blogg.find(params[:id])
+      @blogg = Blogg.friendly.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
