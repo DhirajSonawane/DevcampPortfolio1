24ec0a9c6db7063ea41772ddb4e87b7d76eff7ad
Dhiraj Sonawane <dhiraj.sonawane@coupa.com>
2021-11-16 21:42:22 +0530

    Implemented rails session to share data between pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   include DeviseWhitelist
+
+  before_action :set_sorce
+
+  def set_sorce
+    session[:source] = params[:q] if params[:q]
+  end
 end
